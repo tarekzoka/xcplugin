ce0e0d21670084b57b1aa65d21e77744c3993cf0
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2021-09-14 23:31:36 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -11,6 +11,7 @@ wait
 wget -O /tmp/xcplugin-forever_1.6_all.deb "https://github.com/tarekzoka/xcplugin/raw/main/enigma2-plugin-extensions-xcplugin-forever_1.6_all.ipk"
 
 wait
+ opkg install enigma2-plugin-extensions-xcplugin-forever_1.6_all.ipk
 #!/bin/sh
 opkg update && opkg install --force-overwrite /tmp/*.ipk
 wait
