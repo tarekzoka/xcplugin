83fd57b7a31023a075465a3dfe19fdad47be8bc1
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2021-09-14 23:22:33 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -4,7 +4,7 @@
 # SCRIPT : DOWNLOAD AND INSTALL xcplugin #
 # 
 # Command: wget https://raw.githubusercontent.com/tarekzoka/xcplugin/main/installer.sh -O - | /bin/sh #
-
+wait
 wget -O /tmp/xcplugin-forever_1.6_all.ipk "https://github.com/tarekzoka/xcplugin/blob/main/enigma2-plugin-extensions-xcplugin-forever_1.6_all.ipk"
 
 wait
