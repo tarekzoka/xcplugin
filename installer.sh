20fca78f86e58441537410a476f22fd26f923e10
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2021-09-14 22:38:00 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -8,11 +8,5 @@
 wget -O /tmp/xcplugin-forever_1.6_all.ipk "https://github.com/tarekzoka/xcplugin/raw/main/enigma2-plugin-extensions-xcplugin-forever_1.6_all.ipk"
 
 wait
-opkg install --force-overwrite /var/volatile/tmp/xcplugin-forever_1.6_all.ipk
-wait
-opkg install --force-overwrite /tmp/xcplugin-forever_1.6_all.ipk 
-wait
-opkg update && opkg install --force-overwrite /tmp/*.ipk
-wait
 opkg install /tmp/*.ipk
 sleep 2;
